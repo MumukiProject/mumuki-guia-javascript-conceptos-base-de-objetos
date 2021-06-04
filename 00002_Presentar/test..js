bed3f89fa8941a32637af6d7196505adc96acdab
mayra@mumuki.org <mayra@mumuki.org>
2021-06-04 17:35:38 +0000

    Mumuki Export on 2021-06-04 17:35:38 +0000

--- 00002_Presentar/test..js
+++ 00002_Presentar/test..js
@@ -1,1 +1,23 @@
-TODO 
+describe("Se declaró la variable user", function() {
+  it("", function() {
+    assert(user);
+  })
+})
+
+describe("", function() {
+  it("user tiene la propiedad fullname", function() {
+    assert(user.fullname);
+  })
+})
+
+describe("", function() {
+  it("user tiene la propiedad email", function() {
+    assert(user.email);
+  })
+})
+
+describe("", function() {
+  it("user tiene la propiedad age", function() {
+    assert(user.age);
+  })
+})
