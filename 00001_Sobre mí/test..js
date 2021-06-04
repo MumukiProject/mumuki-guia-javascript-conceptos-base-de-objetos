e9522c15593cb746d53081055dbcfe37772f9ba5
mayra@mumuki.org <mayra@mumuki.org>
2021-06-04 17:06:37 +0000

    Mumuki Export on 2021-06-04 17:06:37 +0000

--- 00001_Sobre mí/test..js
+++ 00001_Sobre mí/test..js
@@ -1,8 +1,8 @@
 describe("", function() {
   it("", function() {
-    assert(sobreMi.includes('nombre'));
-    assert(sobreMi.includes('apellido'));
-    assert(sobreMi.includes('edad'));
+    assert(sobreMi.contains('nombre'));
+    assert(sobreMi.contains('apellido'));
+    assert(sobreMi.contains('edad'));
   })
 })
 describe("", function() {
