e5f9f611da70f501ab2ece216b34a1e8fa6a9de9
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-10 20:23:30 +0000

    Mumuki Export on 2021-06-10 20:23:30 +0000

--- 00003_Canción/test..js
+++ 00003_Canción/test..js
@@ -1,5 +1,5 @@
 describe("", function() {
-  it("Si la canción recibida por parámetro tiene una duration de 166000 la cancion tiene una duracion de 166 segundos", function() {
+  it("Si la canción recibida por parámetro tiene una duration de 166000 la cancion retornada tiene una duracion de 166 segundos", function() {
     let song = {
     title: "Rock and Roll",
     bandName: "Led Zeppelin",
@@ -12,7 +12,7 @@ describe("", function() {
 })
 
 describe("", function() {
-  it("Si song tiene un title 'Shine on you crazy diamond' la cancion tiene como titulo 'Shine on you crazy diamond'", function() {
+  it("Si la canción recibida por parámetro tiene un title 'Shine on you crazy diamond' la cancion retornada tiene como titulo 'Shine on you crazy diamond'", function() {
     song = {
       title: 'Shine on you crazy diamond',
       bandName: "Pink Floyd",
@@ -21,5 +21,18 @@ describe("", function() {
     };
     let resultado = cancion(song);
     assert.equal(resultado.titulo, 'Shine on you crazy diamond');
+  })
+})
+
+describe("", function() {
+  it("Si la canción recibida por parámetro tiene una bandName 'Eruca Sativa' la cancion retornada tiene como banda 'Eruca Sativa'", function() {
+    song = {
+      title: 'Creo',
+      bandName: "Eruca Sativa",
+      duration: 233000,
+      album: "Seremos primavera",
+    };
+    let resultado = cancion(song);
+    assert.equal(resultado.banda, 'Eruca Sativa');
   })
 })
