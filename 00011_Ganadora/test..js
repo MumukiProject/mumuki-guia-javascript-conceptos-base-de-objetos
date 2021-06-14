58d39ce1b7896dab4432e48bc2e4b635ff9098a7
mayra@mumuki.org <mayra@mumuki.org>
2021-06-14 18:11:21 +0000

    Mumuki Export on 2021-06-14 18:11:21 +0000

--- 00011_Ganadora/test..js
+++ 00011_Ganadora/test..js
@@ -1,1 +1,6 @@
-TODO
+describe("", function() {
+  it("Si hacemos ganadoras() nos retorna la lista de ganadoras en el formato correcto", function() {
+    let resultado = ganadoras();
+    assert.equal(resultado, "Lista de Nirvana \n Privada: Sí\n Canciones:\n Smells Like Teen Spirit\nIn Bloom\nCome As You Are\n\n ");
+  })
+})
