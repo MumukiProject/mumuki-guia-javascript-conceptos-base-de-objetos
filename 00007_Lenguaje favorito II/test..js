da2ac8c44cdc439c23b9b3ba2cba7f9cf2cd4c17
mayra@mumuki.org <mayra@mumuki.org>
2021-06-14 14:04:10 +0000

    Mumuki Export on 2021-06-14 14:04:10 +0000

--- 00007_Lenguaje favorito II/test..js
+++ 00007_Lenguaje favorito II/test..js
@@ -1,1 +1,33 @@
-TODO 
+//AGREAGAR MÁS CASOS DE TEST?
+
+describe("Si tenemos un objeto que tiene la propiedad programa: true y hacemos agregarLenguajeFavorito(objeto), la propiedad se agrega al objeto", function() {
+  it("", function() {
+    let datos = {
+      id: 1,
+      nombre: "Wuisti",
+      apellido: "Maggio",
+      email: "wuisti.maggio@gmail.com",
+      telefono: "1234567890",
+      edad: 29,
+      programa: true,
+    };
+    agregarLenguajeFavorito(datos)
+    assert.equal(datos.lenguajeFavorito, 'JavaScript');
+  })
+})
+
+describe("Si tenemos un objeto que tiene la propiedad programa: false y hacemos agregarLenguajeFavorito(objeto), la propiedad no se agrega al objeto", function() {
+  it("", function() {
+    let datos = {
+      id: 1,
+      nombre: "Wuisti",
+      apellido: "Maggio",
+      email: "wuisti.maggio@gmail.com",
+      telefono: "1234567890",
+      edad: 29,
+      programa: false,
+    };
+    agregarLenguajeFavorito(datos)
+    assert.notEqual(datos.lenguajeFavorito, 'JavaScript');
+  })
+})
