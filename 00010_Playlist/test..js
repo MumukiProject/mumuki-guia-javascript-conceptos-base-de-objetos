81425ec753dadbfcd4b3b309ad8e9d9bd32e8cef
mayra@mumuki.org <mayra@mumuki.org>
2021-06-14 18:02:33 +0000

    Mumuki Export on 2021-06-14 18:02:33 +0000

--- 00010_Playlist/test..js
+++ 00010_Playlist/test..js
@@ -6,6 +6,6 @@ describe("", function() {
       canciones: ["Smells Like Teen Spirit", "In Bloom", "Come As You Are"]
     }
     let resultado = infoDeLaPlaylist(playlistDeEjemplo);
-    assert.equal(resultado, "Lista de Nirvana \n Privada: Sí\n Canciones:\n Smells Like Teen Spirit\nIn Bloom\nCome As You Are\n\n ");
+    assert.equal(resultado, "Lista de Nirvana\n Privada: Sí\n Canciones:\nSmells Like Teen Spirit\nIn Bloom\nCome As You Are\n\n ");
   })
 })
