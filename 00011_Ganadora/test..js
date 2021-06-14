e767757e545aef4fbd0422b2cde4626903437613
mayra@mumuki.org <mayra@mumuki.org>
2021-06-14 18:12:48 +0000

    Mumuki Export on 2021-06-14 18:12:48 +0000

--- 00011_Ganadora/test..js
+++ 00011_Ganadora/test..js
@@ -1,6 +1,6 @@
 describe("", function() {
   it("Si hacemos ganadoras() nos retorna la lista de ganadoras en el formato correcto", function() {
     let resultado = ganadoras();
-    assert.equal(resultado, "Lista de Nirvana \n Privada: Sí\n Canciones:\n Smells Like Teen Spirit\nIn Bloom\nCome As You Are\n\n ");
+    assert.equal(resultado, "Bebe Zahara Benet ganó la temporada 1\nTyra Sanchez ganó la temporada 2\nRaja ganó la temporada 3\nSharon Needles ganó la temporada 4\nJinkx Monsoon ganó la temporada 5\nBianca Del Rio ganó la temporada 6\n");
   })
 })
