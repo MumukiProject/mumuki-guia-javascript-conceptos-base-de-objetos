6dcd1a898a2e4f3243385dcae61add06ea89401f
mayra@mumuki.org <mayra@mumuki.org>
2021-06-14 15:33:06 +0000

    Mumuki Export on 2021-06-14 15:33:06 +0000

--- 00012_Canciones/test..js
+++ 00012_Canciones/test..js
@@ -1,38 +1,27 @@
 describe("", function() {
-  it("Si hacemos estanActivas() se agrega Javascript al array de habilidades", function() {
-    let perfil = {
-      id: 123456789,
-      nombre: "Ada Lovelace",
-      url: "https://www.linkedin.com/in/ada-lovelace",
-      habilidades: ["HTML", "CSS", "SASS"],
-    };
-    agregarHabilidad(perfil, 'JavaScript');
-    assert.deepEqual(perfil.habilidades, ['HTML', 'CSS', 'SASS', 'JavaScript']);
+  it("Si hacemos estanActivas() ", function() {
+    let bandas = [
+      { id: 1, nombre: "Nirvana", fundacion: 1987, activa: false },
+      { id: 2, nombre: "Foo Fighters", fundacion: 1994, activa: true },
+      { id: 3, nombre: "Led Zeppelin", fundacion: 1967, activa: false },
+      { id: 3, nombre: "Queens of the Stone Age", fundacion: 1997, activa: true },
+      { id: 3, nombre: "Pearl Jam", fundacion: 1990, activa: true },
+    ];
+    resultado = estanActivas(bandas);
+    assert.equal(resultado, ' ');
   })
 })
 
 describe("", function() {
-  it("Si hacemos agregarHabilidad(perfil, 'Ruby') se agrega Ruby al array de habilidades", function() {
-    let perfil = {
-      id: 123456789,
-      nombre: "Ada Lovelace",
-      url: "https://www.linkedin.com/in/ada-lovelace",
-      habilidades: ["HTML", "CSS", "SASS"],
-    };
-    agregarHabilidad(perfil, 'Ruby');
-    assert.deepEqual(perfil.habilidades, ['HTML', 'CSS', 'SASS', 'Ruby']);
+  it("Si hacemos estanActivas() ", function() {
+    let bandas = [
+      { id: 1, nombre: "Pink Floyd", fundacion: 1964, activa: false },
+      { id: 2, nombre: "Eruca Sativa", fundacion: 2009, activa: true },
+      { id: 3, nombre: "Queen", fundacion: 1970, activa: false },
+      { id: 3, nombre: "The Beatles", fundacion: 1960, activa: false },
+      { id: 3, nombre: "Pearl Jam", fundacion: 1990, activa: true },
+    ];
+    resultado = estanActivas(bandas);
+    assert.equal(resultado, ' ');
   })
 })
-
-describe("", function() {
-  it("Si hacemos agregarHabilidad(perfil, 'React') se agrega React al array de habilidades", function() {
-    let perfil = {
-      id: 123456789,
-      nombre: "Ada Lovelace",
-      url: "https://www.linkedin.com/in/ada-lovelace",
-      habilidades: ["HTML", "CSS", "SASS"],
-    };
-    agregarHabilidad(perfil, 'React');
-    assert.deepEqual(perfil.habilidades, ['HTML', 'CSS', 'SASS', 'React']);
-  })
-}) 
