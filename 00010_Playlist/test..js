b04c37ef2eec07f5f7d332c7b21be0ba5c4a4d8e
mayra@mumuki.org <mayra@mumuki.org>
2021-06-15 14:50:37 +0000

    Mumuki Export on 2021-06-15 14:50:37 +0000

--- 00010_Playlist/test..js
+++ 00010_Playlist/test..js
@@ -1,11 +1,11 @@
 describe("", function() {
   it("ifoDeLaPlaylist me muestra la información de la misma en el formato correcto", function() {
-    let playlistDeEjemplo = {
+    let playlist = {
       nombre: "Lista de Nirvana",
       privada: true,
       canciones: ["Smells Like Teen Spirit", "In Bloom", "Come As You Are"]
     }
-    let resultado = infoDeLaPlaylist(playlistDeEjemplo);
+    let resultado = infoDeLaPlaylist(playlist);
     assert.equal(resultado, "Lista de Nirvana \n Privada: Sí\n Canciones:\n Smells Like Teen Spirit\nIn Bloom\nCome As You Are\n\n "
 );
   })
